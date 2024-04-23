6794082e8870b4623d6c33b2a408929ae197d50c
Samuel Obunge <samuelobunge@yahoo.com>
2024-04-23 08:12:54 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
-"""
-Returns to-do list information for a given employee ID.
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-def fetch_todo_progress(employee_id):
-    """
-    Fetches and displays the to-do list progress for a given employee ID.
-    
-    Args:
-        employee_id (int): The ID of the employee whose to-do list progress is to be fetched.
-    """
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed")]
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    fetch_todo_progress(int(employee_id))
