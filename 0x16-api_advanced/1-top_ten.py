2e0f80afa71723a3b7976192a16f51893e156672
Samuel Obunge <samuelobunge@yahoo.com>
2024-05-08 13:00:01 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Sobunge)"
+        "User-Agent": "linux:0x16.api.advanced_test:v1.0.0 (by /u/sobunge)"
     }
     params = {
         "limit": 10
