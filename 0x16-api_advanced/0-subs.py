c798adfb217ad529a2e456fc4a0e90f4a6880e22
Samuel Obunge <samuelobunge@yahoo.com>
2024-05-08 08:07:59 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-"""
-Module Name: 0-subs.py
-Description: This module provides a function to query the number of subscribers for a given subreddit.
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}  # Reddit API requires a user-agent header
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
-
-if __name__ == "__main__":
-    subreddit = "programming"
-    print(number_of_subscribers(subreddit))
+    results = response.json().get("data")
+    return results.get("subscribers")
