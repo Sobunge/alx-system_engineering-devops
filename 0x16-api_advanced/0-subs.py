79430f1f2a83bb7a2d392edd20f5263c47a10709
Samuel Obunge <samuelobunge@yahoo.com>
2024-05-08 12:51:03 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,18 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/sobunge)"
     }
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
+        
+        # Access rate limit headers
+        ratelimit_used = int(response.headers.get('X-Ratelimit-Used', 0))
+        ratelimit_remaining = int(response.headers.get('X-Ratelimit-Remaining', 0))
+        ratelimit_reset = int(response.headers.get('X-Ratelimit-Reset', 0))
+        
+        print(f"Rate limit used: {ratelimit_used}")
+        print(f"Rate limit remaining: {ratelimit_remaining}")
+        print(f"Rate limit reset (seconds): {ratelimit_reset}")
+        
         data = response.json().get("data")
         return data.get("subscribers")
     except requests.exceptions.HTTPError as e:
