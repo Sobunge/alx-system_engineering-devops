a3a31fedf588baa92ef61881c20d0cd8cd197c3c
Samuel Obunge <samuelobunge@yahoo.com>
2024-05-08 11:58:57 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
+        headers={"User-Agent": "Template/1.0 by /u/sobunge"},
     )
 
     if req.status_code == 200:
