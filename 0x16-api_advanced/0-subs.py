5a2a9470f2dac86c935a92cb9ac289cdea44ac6f
Samuel Obunge <samuelobunge@yahoo.com>
2024-05-08 13:04:52 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:5.3.0-1032-aws:v1.0.0 (by /u/sobunge_)"
+        "User-Agent": "Mozilla/5.0""
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
