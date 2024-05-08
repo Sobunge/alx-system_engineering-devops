651b2a65f67ad9fd8e95cd7985c487a256c51c50
Samuel Obunge <samuelobunge@yahoo.com>
2024-05-08 12:54:56 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/sobunge)"
-    }
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
-        
-        # Access rate limit headers
-        ratelimit_used = int(response.headers.get('X-Ratelimit-Used', 0))
-        ratelimit_remaining = int(response.headers.get('X-Ratelimit-Remaining', 0))
-        ratelimit_reset = int(response.headers.get('X-Ratelimit-Reset', 0))
-        
-        print(f"Rate limit used: {ratelimit_used}")
-        print(f"Rate limit remaining: {ratelimit_remaining}")
-        print(f"Rate limit reset (seconds): {ratelimit_reset}")
-        
-        data = response.json().get("data")
-        return data.get("subscribers")
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error occurred: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-    return 0
 
-print(number_of_subscribers("programming"))
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
